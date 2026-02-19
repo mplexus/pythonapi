5bfdb6ecee42c8434740b6fd048e63cba79b4a93
Michael Plexousakis <adm_micple01@cramo.com>
2026-02-20 00:43:08 +0100

    Added permissions

--- mplexus_api/views.py
+++ mplexus_api/views.py
@@ -6,6 +6,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from mplexus_api import serializers
 from mplexus_api import models
@@ -114,14 +116,16 @@ class UserLoginApiView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-
 class UserProfileFeedViewSet(viewsets.ModelViewSet):
     """Create and update user feed items"""
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated
+    )
 
     def perform_create(self, serializer):
         """Sets the user profile to the logged in user"""
         serializer.save(user_profile=self.request.user)
-        
